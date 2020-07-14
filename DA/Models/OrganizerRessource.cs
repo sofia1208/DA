58c0f48cbc188e4309a255d70e7a1fbbbe1f0ae1
iarthofer16 <47629174+iarthofer16@users.noreply.github.com>
2020-07-14 13:08:13 +0200

    get lists from database

---
+++ DA/Models/OrganizerRessource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schulungskalender.Models {
+    public class OrganizerRessource {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ContactPerson { get; set; }
+        public string Email { get; set; }
+        public string Website { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
---
+++ DA/Models/RegistrationRessource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schulungskalender.Models {
+    public class RegistrationRessource {
+        public int SchoolingId { get; set; }
+        public int PersonId { get; set; }
+    }
+}
