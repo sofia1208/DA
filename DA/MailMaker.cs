e82308989672d687372a79b4f1b11973e73d3aa3
iarthofer16 <47629174+iarthofer16@users.noreply.github.com>
2020-07-15 13:44:11 +0200

    code cleanup

--- DA/MailMaker.cs
+++ DA/MailMaker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Mail;
 using System.Threading.Tasks;
 
@@ -10,105 +11,28 @@ namespace DA {
         public MailMaker() {
         }
 
-        public string Absender {
-            get;
-            set;
-        }
+        public void sendMail() {
+            try {
+                var smtpClient = new SmtpClient("smtp.gmail.com") {
+                    Port = 587,
+                    UseDefaultCredentials = false,
+                    Credentials = new NetworkCredential("isabelle.arthofer@gmail.com", "3dfj#8JkIA"),
+                    EnableSsl = true,
+                };
 
+                var mailMessage = new MailMessage {
+                    From = new MailAddress("isabelle.arthofer@gmail.com"),
+                    Subject = "Test",
+                    Body = "<h1>Test</h1>",
+                    IsBodyHtml = true,
+                };
+                mailMessage.To.Add("isi.gaubinger@gmail.com");
 
-        public List<string> Empfänger {
-            get;
-            set;
-        }
-
-
-        public List<string> Kopie {
-            get;
-            set;
-        }
-
-
-        public List<string> Blindkopie {
-            get;
-            set;
-        }
-
-
-        public List<Attachment> Anhänge {
-            get;
-            set;
-        }
-
-
-        public string Betreff {
-            get;
-            set;
-        }
-
-
-        public string Nachricht {
-            get;
-            set;
-        }
-
-
-        public string Username {
-            get;
-            set;
-        }
-
-
-        public string Passwort {
-            private get;
-            set;
-        }
-
-
-        public string Servername {
-            get;
-            set;
-        }
-
-
-        public string Port {
-            get;
-            set;
-        }
-
-        public void Send() {
-            MailMessage Email = new MailMessage();
-
-            MailAddress Sender = new MailAddress(Absender);
-            Email.From = Sender;
-
-            foreach (string empf in Empfänger)
-                Email.To.Add(empf);
-
-            if (Kopie.Count != 0)
-                foreach (string kopie in Kopie)
-                    Email.CC.Add(kopie);
-
-            if (Blindkopie.Count != 0)
-                foreach (string blindkopie in Blindkopie)
-                    Email.Bcc.Add(blindkopie);
-
-            if (Anhänge.Count != 0)
-                foreach (Attachment anhang in Anhänge)
-                    Email.Attachments.Add(anhang);
-
-            Email.Subject = Betreff;
-
-            Email.Body = Nachricht;
-
-            SmtpClient MailClient = new SmtpClient(Servername, int.Parse(Port));
-
-            string UserName = Username;
-            string Password = Passwort;
-            System.Net.NetworkCredential Credentials = new System.Net.NetworkCredential(UserName, Password);
-
-            MailClient.Credentials = Credentials;
-
-            MailClient.Send(Email);
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
--- DA/Services/SchoolingService.cs
+++ DA/Services/SchoolingService.cs
@@ -9,10 +9,13 @@ using System.Data;
 using MySql.Data.MySqlClient;
 using System.Text;
 using System.Net;
+using System.Diagnostics;
 
 namespace Schulungskalender.Services {
     public class SchoolingService {
-        RessourceDtoConverter converter;
+        private readonly RessourceDtoConverter converter;
+        private readonly MailMaker mailMaker;
+        private readonly Database db;
 
         private List<AddressRessource> addresses;
         private List<CompanyRessource> companies;
@@ -24,16 +27,11 @@ namespace Schulungskalender.Services {
 
         public SchoolingService() {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-
-            addresses = new List<AddressRessource>();
-            companies = new List<CompanyRessource>();
-            organizers = new List<OrganizerRessource>();
-            persons = new List<PersonRessource>();
-            registrations = new List<RegistrationRessource>();
-            schoolings = new List<SchoolingRessource>();
-            fillLists();
-
             converter = new RessourceDtoConverter();
+            mailMaker = new MailMaker();
+            db = new Database();
+            
+            fillLists();
         }
 
         public List<SchoolingSummaryDTO> Summary(string type) {
@@ -45,29 +43,6 @@ namespace Schulungskalender.Services {
         }
 
         public SchoolingDetailDTO GetDetails(int id) {
-
-            try {
-                var smtpClient = new SmtpClient("smtp.gmail.com") {
-                    Port = 587,
-                    UseDefaultCredentials = false,
-                    Credentials = new NetworkCredential("isabelle.arthofer@gmail.com", "3dfj#8JkIA"),
-                    EnableSsl = true,
-                };
-
-                var mailMessage = new MailMessage {
-                    From = new MailAddress("isabelle.arthofer@gmail.com"),
-                    Subject = "Test",
-                    Body = "<h1>Test</h1>",
-                    IsBodyHtml = true,
-                };
-                mailMessage.To.Add("isi.gaubinger@gmail.com");
-
-                smtpClient.Send(mailMessage);
-            }
-            catch (Exception e) {
-                Console.WriteLine(e.Message);
-            }
-
             var schooling = schoolings.Find(x => x.Id == id);
             var address = addresses.Find(x => x.Id == schooling.AddressId);
             var organizer = organizers.Find(x => x.Id == schooling.OrganizerId);
@@ -76,74 +51,20 @@ namespace Schulungskalender.Services {
         }
 
         public RegistrationDTO Register(RegistrationDTO registration) {
+            mailMaker.sendMail();
+
             return registration;
         }
 
         private void fillLists() {
-            var con = "Server=10.90.90.222;Database=educationPlanner;Uid=ep;Pwd=eDpL2%0!;persistsecurityinfo=True";
-            var db = new MySqlConnection(con);
-            db.Open();
+            addresses = new List<AddressRessource>();
+            companies = new List<CompanyRessource>();
+            organizers = new List<OrganizerRessource>();
+            persons = new List<PersonRessource>();
+            registrations = new List<RegistrationRessource>();
+            schoolings = new List<SchoolingRessource>();
 
-            var getAddressesCmd = new MySqlCommand("SELECT * FROM addresses", db);
-
-            AddressRessource address;
-            using (MySqlDataReader reader = getAddressesCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    address = new AddressRessource() { Id = reader.GetInt32(0), Street = reader.GetString(1) ?? "", StreetNumber = reader.GetInt32(2), ZipCode = reader.GetInt32(3), City = reader.GetString(4), Country = reader.GetString(5) };
-                    addresses.Add(address);
-                }
-            }
-
-            var getCompaniesCmd = new MySqlCommand("SELECT * FROM companies", db);
-
-            CompanyRessource company;
-            using (MySqlDataReader reader = getCompaniesCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    company = new CompanyRessource() { Id = reader.GetInt32(0), Name = reader.GetString(1), Phone = reader.GetString(2), Email = reader.GetString(3), AddressId = reader.GetInt32(4) };
-                    companies.Add(company);
-                }
-            }
-
-            var getOrganizersCmd = new MySqlCommand("SELECT * FROM organizers", db);
-
-            OrganizerRessource organizer;
-            using (MySqlDataReader reader = getOrganizersCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    organizer = new OrganizerRessource() { Id = reader.GetInt32(0), Name = reader.GetString(1), ContactPerson = reader.GetString(2), Email = reader.GetString(3), Website = reader.GetString(4), Phone = reader.GetString(5) };
-                    organizers.Add(organizer);
-                }
-            }
-
-            var getPersonsCmd = new MySqlCommand("SELECT * FROM persons", db);
-
-            PersonRessource person;
-            using (MySqlDataReader reader = getPersonsCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    person = new PersonRessource() { Id = reader.GetInt32(0), Firstname = reader.GetString(1), Lastname = reader.GetString(2), Email = reader.GetString(3), CompanyId = reader.GetInt32(4) };
-                    persons.Add(person);
-                }
-            }
-
-            var getSchoolingsCmd = new MySqlCommand("SELECT * FROM schoolings", db);
-
-            SchoolingRessource schooling;
-            using (MySqlDataReader reader = getSchoolingsCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    bool reservation = (reader.GetInt32(5) == 1) ? true : false;
-                    schooling = new SchoolingRessource() { Id = reader.GetInt32(0), Name = reader.GetString(1), AddressId = reader.GetInt32(2), Start = reader.GetDateTime(3), End = reader.GetDateTime(4), Reservation = reservation, ReservationDate = reader.GetDateTime(6), OrganizerId = reader.GetInt32(7), Places = reader.GetInt32(8), Price = reader.GetDouble(9) };
-                    schoolings.Add(schooling);
-                }
-            }
-
-            var getRegistrationCmd = new MySqlCommand("SELECT * FROM registrations", db);
-
-            RegistrationRessource registration;
-            using (MySqlDataReader reader = getRegistrationCmd.ExecuteReader()) {
-                while (reader.Read()) {
-                    registration = new RegistrationRessource() { SchoolingId = reader.GetInt32(0), PersonId = reader.GetInt32(1) };
-                    registrations.Add(registration);
-                }
-            }
+            db.getAllTables(ref addresses, ref companies, ref schoolings, ref organizers, ref registrations, ref persons);
         }
 
         private bool IsSchoolingFree(int id) {
