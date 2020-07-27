47a1f1adbd2dd6287cc1684ddb7c53a7cecc9259
iarthofer16 <47629174+iarthofer16@users.noreply.github.com>
2020-07-27 08:52:28 +0200

    Code cleanup, BackendController/-Service

---
+++ DA/Controllers/BackendController.cs
@@ -0,0 +1,35 @@
+﻿using DA.Models.DTOs;
+using DA.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class BackendController {
+        private readonly BackendService backendService;
+
+        public BackendController(BackendService schoolingService) {
+            this.backendService = schoolingService;
+        }
+
+        [HttpGet("Schoolings")]
+        public void Schoolings() {
+            backendService.GetSchoolings();
+        }
+
+        [HttpGet("Schoolings/{id}")]
+        public BackendDetailDTO Schoolings(int id) {
+            return backendService.GetSchoolings(id);
+        }
+
+        [HttpPut("EditSchooling/{id}")]
+        public string EditSchooling(int id, [FromBody] BackendDetailDTO schooling) {
+            return backendService.EditSchooling(schooling);
+        }
+
+    }
+}
---
+++ DA/Services/BackendService.cs
@@ -0,0 +1,49 @@
+﻿using DA.Models.DTOs;
+using Schulungskalender.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DA.Services {
+    public class BackendService {
+        private readonly Database db;
+
+        private List<AddressRessource> addresses;
+        private List<CompanyRessource> companies;
+        private List<OrganizerRessource> organizers;
+        private List<PersonRessource> persons;
+        private List<RegistrationRessource> registrations;
+        private List<SchoolingRessource> schoolings;
+
+        public BackendService() {
+            db = new Database();
+
+            fillLists();
+        }
+
+        public void GetSchoolings() {
+
+        }
+
+        public BackendDetailDTO GetSchoolings(int id) {
+            return null;
+        }
+
+        public string EditSchooling(BackendDetailDTO schooling) {
+            return "test successfull";
+        }
+
+        private void fillLists() {
+            addresses = new List<AddressRessource>();
+            companies = new List<CompanyRessource>();
+            organizers = new List<OrganizerRessource>();
+            persons = new List<PersonRessource>();
+            registrations = new List<RegistrationRessource>();
+            schoolings = new List<SchoolingRessource>();
+
+            db.getAllTables(ref addresses, ref companies, ref schoolings, ref organizers, ref registrations, ref persons);
+        }
+
+    }
+}
