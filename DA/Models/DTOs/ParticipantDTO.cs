6a5f2584ed3d77475a02a5995fb9d64e21539a7f
iarthofer16 <47629174+iarthofer16@users.noreply.github.com>
2020-07-28 11:18:55 +0200

    fixed some merge issues

--- DA/Models/DTOs/ParticipantDTO.cs
+++ DA/Models/DTOs/ParticipantDTO.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 namespace DA.Models.DTOs {
     public class ParticipantDTO {
+        public int Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
         public string Email { get; set; }
