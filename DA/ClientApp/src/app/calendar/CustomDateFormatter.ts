6af60fbeb1f5dbf73fd762e6e1f1938f40f63dee
sofia1208 <sofia.schmidmair@live.de>
2020-11-21 11:28:11 +0100

    austrian date

--- DA/ClientApp/src/app/calendar/CustomDateFormatter.ts
+++ DA/ClientApp/src/app/calendar/CustomDateFormatter.ts
@@ -1,4 +1,4 @@
-import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
+import { CalendarDateFormatter, DateFormatterParams, getWeekViewPeriod } from 'angular-calendar';
 import { DatePipe } from '@angular/common';
 import { Injectable } from '@angular/core';
 
@@ -12,5 +12,35 @@ export class CustomDateFormatter extends CalendarDateFormatter {
 
   public weekViewHour({ date, locale }: DateFormatterParams): string {
     return this.dayViewHour({ date, locale });
+  }
+
+  public dayViewTitle({ date, locale }: DateFormatterParams): string {
+    return new DatePipe(locale).transform(date, 'dd. MMMM yyyy', locale);
+  }
+
+  public weekViewTitle({ date, locale }: DateFormatterParams): string {
+   
+    const { viewStart, viewEnd } = getWeekViewPeriod(
+      this.dateAdapter,
+      date,
+      1,
+      [],
+      0
+    );
+   
+    console.log( this.dateAdapter.startOfWeek(date, {weekStartsOn: 1}));
+    const format = (dateToFormat: Date, showYear: boolean) =>
+      new Intl.DateTimeFormat(locale, {
+       
+        month: 'short',
+        day: 'numeric',
+        year: showYear ? 'numeric' : undefined,
+      }).format(dateToFormat);
+
+    return `${format(
+      viewStart,
+      viewStart.getUTCFullYear() !== viewEnd.getUTCFullYear()
+    )} - ${format(viewEnd, true)}`;
+   
   }
 }
