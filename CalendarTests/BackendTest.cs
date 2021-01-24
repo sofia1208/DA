2c9e4cc16141110e8e4bf744f1b008f22d30594c
iarthofer16 <47629174+iarthofer16@users.noreply.github.com>
2021-01-24 16:35:20 +0100

    more tests

--- CalendarTests/BackendTest.cs
+++ CalendarTests/BackendTest.cs
@@ -11,7 +11,6 @@ namespace CalendarTests {
         [TestMethod]
         public void TestGetSchoolings() {
             var schoolings = backendService.GetSchoolings();
-            schoolings.ForEach(x => Assert.IsTrue(x.Display));
             Assert.IsTrue(schoolings.Count != 0);
         }
     }
